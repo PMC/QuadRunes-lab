aebb5a7b2437dbcd26a7e5dde16892a719713148
Pierre Chateau <pierre@chateau.se>
2025-04-23 01:20:55 +0200

    live update screen every 1 sec

--- src/main.py
+++ src/main.py
@@ -8,32 +8,35 @@ from rich.table import Table
 from time import sleep
 from rich.align import Align
 from rich.prompt import Confirm
+from rich.live import Live
+import random
+
+
+def generate_game_map() -> Layout:
+    """Make a new game area."""
+    global score_a
+    layout = Layout()
+    layout.split_column(
+        Layout(name="score_top", size=3), Layout(name="game_area", size=35)
+    )
+    value = random.random() * 100
+    score_a += value
+    layout["score_top"].split_row(
+        Layout(Panel(f"Score A: {score_a}"), name="score_a"),
+        Layout(Panel(Text("Score B: 15", justify="right")), name="score_b"),
+    )
+    return layout
 
 
 def main():
-    console = Console(height=33)
-    with console.screen(style="bold white on black") as screen:
-        # screen.console.print(            "Hello, [bold magenta]World[/bold magenta]!", ":apple:", locals())
-
-        layout = Layout()
-        layout.split_column(Layout(name="score_top", size=3), Layout(name="game_area"))
-        layout["score_top"].split_row(
-            Layout(Panel("Score A: 200"), name="score_a"),
-            Layout(Panel(Text("Score B: 15", justify="right")), name="score_b"),
-        )
-
-        screen.console.print(layout)
-        screen.console.print(layout.tree)
-        grid = Table.grid(expand=True)
-        grid.add_column()
-        grid.add_column(justify="right")
-        grid.add_row(
-            "Raising shields", "[bold magenta]COMPLETED [green]:heavy_check_mark:"
-        )
-
-        screen.console.print(grid)
-        is_rich_great = Confirm.ask("Do you like rich?")
+    with Live(
+        generate_game_map(), refresh_per_second=1, transient=True, screen=True
+    ) as live:
+        for _ in range(40):
+            sleep(0.4)
+            live.update(generate_game_map())
 
 
 if __name__ == "__main__":
+    score_a = 0
     main()
